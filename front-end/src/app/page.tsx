ec1d96d0bc2272414b0381adeb886ea953edeaf1
Clara Genovese <clariigenovese@gmail.com>
2024-06-22 18:49:19 -0300

    First commit: boilerplate

---
+++ front-end/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Header from "./components/Header";
+
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Página Principal</h2>
+      <p>Bienvenido a nuestra App !!!!</p>
+    </div>
+  );
+}
